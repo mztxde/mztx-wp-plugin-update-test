e503eeaa6c882ba976c734da97610b38de2459ef
Michael Zapf <m.zapf@mztx.de>
2024-12-20 11:13:34 +0100

    first try

---
+++ build_plugin_manifest.php
@@ -0,0 +1,12 @@
+<?php
+
+$infilePath = $argv[1];
+
+$manifest = json_decode(file_get_contents($infilePath), true);
+$manifest['version'] = getenv('VERSION');
+$manifest['download_url'] = getenv('DOWNLOAD_URL');
+$manifest['last_updated'] = (new DateTimeImmutable())->format('Y-m-d H:i:s');
+$manifest['sections']['changelog'] = 'TODO';
+
+echo json_encode($manifest, JSON_PRETTY_PRINT);
+echo PHP_EOL;
---
+++ wp-content/plugins/mztx-wp-plugin-update-test/mztx-wp-plugin-update-test.php
@@ -0,0 +1,17 @@
+<?php
+
+/**
+ * The plugin bootstrap file
+ *
+ * @wordpress-plugin
+ * Plugin Name:       mztx Plugin Update Test
+ * Plugin URI:        https://mztx.de
+ * Description:       Nur ein Test. Hat keine sinnvolle Funktionalität, außer, dass es sich selbst updaten kann.
+ * Version:           {{ version    }}
+ * Author:            Michael Zapf
+ * Author URI:        https://mztx.de
+ * License:           BSD-3-Clause
+ * License URI:       https://raw.githubusercontent.com/mztxde/mztx-wp-plugin-update-test/refs/heads/main/LICENSE
+ */
+
+declare(strict_types=1);
