26747588d3cc369e75cf57157e53b635269d5494
Michael Zapf <m.zapf@mztx.de>
2024-12-20 11:45:25 +0100

    add updater class first try

--- wp-content/plugins/mztx-wp-plugin-update-test/mztx-wp-plugin-update-test.php
+++ wp-content/plugins/mztx-wp-plugin-update-test/mztx-wp-plugin-update-test.php
@@ -7,7 +7,7 @@
  * Plugin Name:       mztx Plugin Update Test
  * Plugin URI:        https://mztx.de
  * Description:       Nur ein Test. Hat keine sinnvolle Funktionalität, außer, dass es sich selbst updaten kann.
- * Version:           {{ version    }}
+ * Version:           {{ version }}
  * Author:            Michael Zapf
  * Author URI:        https://mztx.de
  * License:           BSD-3-Clause
@@ -15,3 +15,6 @@
  */
 
 declare(strict_types=1);
+
+const MZTX_WP_PLUGIN_UPDATE_TEST_PLUGIN_MANIFEST_URL = 'https://mztxde.github.io/mztx-wp-plugin-update-test/manifest.json';
+const MZTX_WP_PLUGIN_UPDATE_TEST_PLUGIN_VERSION = '{{ version }}';
