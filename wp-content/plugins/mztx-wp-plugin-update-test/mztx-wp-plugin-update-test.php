a273a8d9c24195fa965d49c6c9f48e850a3ed00e
Michael Zapf <m.zapf@mztx.de>
2024-12-20 12:47:25 +0100

    next try, different approach

--- wp-content/plugins/mztx-wp-plugin-update-test/mztx-wp-plugin-update-test.php
+++ wp-content/plugins/mztx-wp-plugin-update-test/mztx-wp-plugin-update-test.php
@@ -10,15 +10,18 @@
  * Version:           {{ version }}
  * Author:            Michael Zapf
  * Author URI:        https://mztx.de
+ * Update URI:        https://mztxde.github.io/mztx-wp-plugin-update-test/manifest.json
  * License:           BSD-3-Clause
  * License URI:       https://raw.githubusercontent.com/mztxde/mztx-wp-plugin-update-test/refs/heads/main/LICENSE
  */
 
 declare(strict_types=1);
 
-const MZTX_WP_PLUGIN_UPDATE_TEST_PLUGIN_MANIFEST_URL = 'https://mztxde.github.io/mztx-wp-plugin-update-test/manifest.json';
 const MZTX_WP_PLUGIN_UPDATE_TEST_PLUGIN_VERSION = '{{ version }}';
 
 require_once __DIR__ . '/update.php';
 
-$mztxWpPluginUpdateTestUpdater = new \mztx\WpPluginUpdateTest\Updater();
+$mztxWpPluginUpdateTestUpdater = new \mztx\WpPluginUpdateTest\Updater(
+    \basename(__DIR__),
+    MZTX_WP_PLUGIN_UPDATE_TEST_PLUGIN_VERSION,
+);
--- wp-content/plugins/mztx-wp-plugin-update-test/update.php
+++ wp-content/plugins/mztx-wp-plugin-update-test/update.php
@@ -5,130 +5,26 @@ declare(strict_types=1);
 namespace mztx\WpPluginUpdateTest;
 
 /**
- * Based on https://anchor.host/using-github-to-self-host-updates-for-wordpress-plugins/
+ * Based on https://webfoersterei.de/blog/wordpress-plugins-update-hook/
  */
-class Updater
+readonly class Updater
 {
-    private string $plugin_slug;
-    private string $version;
-    private string $cache_key;
-    private bool $cache_allowed;
-
-    public function __construct()
-    {
-        $this->plugin_slug = dirname(plugin_basename(__DIR__));
-        $this->version = MZTX_WP_PLUGIN_UPDATE_TEST_PLUGIN_VERSION;
-        $this->cache_key = $this->plugin_slug . '_updater';
-        $this->cache_allowed = false;
-
-        add_filter('plugins_api', [$this, 'info'], 20, 3);
-        add_filter('site_transient_update_plugins', [$this, 'update']);
-        add_action('upgrader_process_complete', [$this, 'purge'], 10, 2);
+    public function __construct(
+        private string $slug,
+        private string $version,
+    ) {
+        add_filter('update_plugins_mztxde.github.io', [$this, 'plugin_update_handler'], 10, 4);
     }
 
-    public function request(): mixed
+    public function plugin_update_handler($updateInfo, $pluginHeaders, $pluginFile, $language)
     {
-        $remote = get_transient($this->cache_key);
-
-        if (false === $remote || !$this->cache_allowed) {
-            $remote = wp_remote_get(MZTX_WP_PLUGIN_UPDATE_TEST_PLUGIN_MANIFEST_URL, [
-                    'timeout' => 10,
-                    'headers' => [
-                        'Accept' => 'application/json',
-                    ],
-                ],
-            );
-
-            if (is_wp_error($remote) || 200 !== wp_remote_retrieve_response_code($remote) || empty(wp_remote_retrieve_body($remote))) {
-                return false;
-            }
-
-            set_transient($this->cache_key, $remote, DAY_IN_SECONDS);
+        $requestedSlug = basename($pluginFile);
+        if ($requestedSlug !== $this->slug) {
+            return false;
         }
 
-        return json_decode(wp_remote_retrieve_body($remote));
-
-    }
-
-    function info($response, $action, $args): mixed
-    {
-        // do nothing if you're not getting plugin information right now
-        if ('plugin_information' !== $action) {
-            return $response;
-        }
-
-        // do nothing if it is not our plugin
-        if (empty($args->slug) || $this->plugin_slug !== $args->slug) {
-            return $response;
-        }
-
-        // get updates
-        $remote = $this->request();
-
-        if (!$remote) {
-            return $response;
-        }
-
-        $response = new \stdClass();
-
-        $response->name = $remote->name;
-        $response->slug = $remote->slug;
-        $response->version = $remote->version;
-        $response->tested = $remote->tested;
-        $response->requires = $remote->requires;
-        $response->author = $remote->author;
-        $response->author_profile = $remote->author_profile;
-        $response->donate_link = $remote->donate_link;
-        $response->homepage = $remote->homepage;
-        $response->download_link = $remote->download_url;
-        $response->trunk = $remote->download_url;
-        $response->requires_php = $remote->requires_php;
-        $response->last_updated = $remote->last_updated;
-
-        $response->sections = [
-            'description' => $remote->sections->description,
-            'installation' => $remote->sections->installation,
-            'changelog' => $remote->sections->changelog
-        ];
-
-        if (!empty($remote->banners)) {
-            $response->banners = [
-                'low' => $remote->banners->low,
-                'high' => $remote->banners->high
-            ];
-        }
-
-        return $response;
-
-    }
-
-    public function update($transient): mixed
-    {
-        if (empty($transient->checked)) {
-            return $transient;
-        }
-
-        $remote = $this->request();
-        if ($remote && version_compare($this->version, $remote->version, '<') && version_compare($remote->requires, get_bloginfo('version'), '<=') && version_compare($remote->requires_php, PHP_VERSION, '<')) {
-            $response = new \stdClass();
-            $response->slug = $this->plugin_slug;
-            $response->plugin = "{$this->plugin_slug}/{$this->plugin_slug}.php";
-            $response->new_version = $remote->version;
-            $response->tested = $remote->tested;
-            $response->package = $remote->download_url;
-
-            $transient->response[$response->plugin] = $response;
-
-        }
-
-        return $transient;
-    }
-
-    public function purge($upgrader, $options): void
-    {
-        if ($this->cache_allowed && 'update' === $options['action'] && 'plugin' === $options['type']) {
-            // just clean the cache when new plugin version is installed
-            delete_transient($this->cache_key);
-        }
+        // @TODO Error handling etc.
+        $response = file_get_contents($pluginHeaders['UpdateURI']);
+        return json_decode($response);
     }
 }
